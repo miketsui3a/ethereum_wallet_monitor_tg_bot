db22f842b247ebecd5b809a202ea4eae25e2a134
miketsui3a <42023809+miketsui3a@users.noreply.github.com>
2021-10-12 19:14:00 +0800

    asdf

--- app.js
+++ app.js
@@ -12,161 +12,239 @@ const routerAbi = require("./routerAbi.json");
 const targetAddressJson = require("./targetAddress.json");
 
 const TelegramBot = require("node-telegram-bot-api");
+// let web4 = new Web3(process.env.WEB3_WEBSOCKET_ENDPOINT);
+let web4 = new Web3(process.env.WEB3_HTTP_ENDPOINT);
 
 const bot = new TelegramBot(process.env.TG_BOT_API_KEY, { polling: true });
 
 abiDecoder.addABI(erc20Abi);
 abiDecoder.addABI(routerAbi);
 
+async function scanBlock(blockNumber, myCache){
+  let targetAddress = myCache.get("targetAddress");
+  const block = await web4.eth.getBlock(blockNumber)
+  console.log(block.transactions)
+  console.log(targetAddress)
+  block.transactions.map(async txHash=>{
+    const tx = await web4.eth.getTransaction(txHash)
+    const receipt = await web4.eth.getTransactionReceipt(txHash)
+    try {
+      if (targetAddress.map((x) => x.address).includes(tx.from)) {
+        const target = targetAddress.filter((x) => x.address == tx.from)[0];
+
+        console.log("bingo!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+
+        const data = abiDecoder.decodeMethod(tx.input);
+        if (receipt.status && data && data.name.includes("swap")) {
+          let logs = abiDecoder.decodeLogs(receipt.logs);
+          logs = logs.filter((log) => log.name == "Transfer");
+          console.log(tx.from);
+
+          const path =
+            data.params[data.params.findIndex((x) => x.name == "path")].value;
+          const inTokenContract = new web4.eth.Contract(erc20Abi, path[0]);
+          const outTokenContract = new web4.eth.Contract(
+            erc20Abi,
+            path[path.length - 1]
+          );
+
+          const inTokenName = await inTokenContract.methods.symbol().call();
+          const outTokenName = await outTokenContract.methods.symbol().call();
+
+          const inAmount =
+            inTokenName == "USDC" || inTokenName == "fUSDT"
+              ? web4.utils.fromWei(logs[0].events[2].value, "mwei")
+              : web4.utils.fromWei(logs[0].events[2].value, "ether");
+          const outAmount =
+            outTokenName == "USDC" || outTokenName == "fUSDT"
+              ? web4.utils.fromWei(
+                  logs[logs.length - 1].events[2].value,
+                  "mwei"
+                )
+              : web4.utils.fromWei(
+                  logs[logs.length - 1].events[2].value,
+                  "ether"
+                );
+          console.log("intoken: ", inTokenName, inAmount);
+          console.log("outToken: ", outTokenName, outAmount);
+
+          const targetGroupIds = require("./groupIds.json");
+
+          for (id of targetGroupIds) {
+            bot.sendMessage(
+              id,
+              `${target.name}\n${target.address} \nsell: ${inTokenName} ${inAmount}\nbuy: ${outTokenName} ${outAmount}\nhttps://ftmscan.com/tx/${txHash}`
+            );
+          }
+        }
+      }
+    } catch (e) {
+      console.log(e);
+    }
+
+  })
+}
+
 async function main() {
-    let web3 = new Web3(process.env.WEB3_WEBSOCKET_ENDPOINT);
-    let web4 = new Web3(process.env.WEB3_HTTP_ENDPOINT);
-    console.log("running...");
+  console.log("running...");
 
-    myCache.set("targetAddress", targetAddressJson);
+  myCache.set("targetAddress", targetAddressJson);
 
-    myCache.on("expired", function (key, value) {
-        console.log("cache expired");
-        myCache.set("targetAddress", value);
-        fs.writeFileSync("./targetAddress.json", JSON.stringify(value));
-        console.log("cache updated");
-        console.log(myCache.get("targetAddress"));
-    });
+  myCache.on("expired", function (key, value) {
+    console.log("cache expired");
+    myCache.set("targetAddress", value);
+    fs.writeFileSync("./targetAddress.json", JSON.stringify(value));
+    console.log("cache updated");
+  });
 
-    bot.on("message", (msg) => {
-        const chatId = msg.chat.id;
-        console.log(msg);
+  bot.on("message", (msg) => {
+    const chatId = msg.chat.id;
+    console.log(msg);
 
-        fs.readFile("./groupIds.json", "utf8", (err, data) => {
-            if (err) {
-                console.log(err);
-            } else {
-                const json = JSON.parse(data);
-                if (!json.includes(chatId)) {
-                    json.push(chatId);
-                    fs.writeFileSync("./groupIds.json", JSON.stringify(json));
-                }
-            }
-        });
-
-        if (msg.text) {
-            const tokenized = msg.text.split(" ");
-            if (tokenized[0] == "/add") {
-                if (tokenized.length != 3) {
-                    bot.sendMessage(
-                        chatId,
-                        "invalid command i.e. /add ElonMusk 0x4d9361a86d038c8ada3db2457608e2275b3e08d4"
-                    );
-                } else if (
-                    myCache.get("targetAddress").filter((x) => x.address == tokenized[2])
-                        .length != 0
-                ) {
-                    bot.sendMessage(chatId, "address already in database");
-                } else if (!web3.utils.isAddress(tokenized[2])) {
-                    bot.sendMessage(chatId, "invalid address");
-                } else {
-                    const cache = myCache.get("targetAddress");
-                    cache.push({
-                        name: tokenized[1],
-                        address: tokenized[2],
-                    });
-                    myCache.set("targetAddress", cache);
-                    console.log("address: ", tokenized[2], " added!");
-                    bot.sendMessage(chatId, "address added!");
-                }
-            } else if (tokenized[0] == "/delete") {
-                if (tokenized.length != 2) {
-                    bot.sendMessage(
-                        chatId,
-                        "invalid command i.e. /delete 0x4d9361a86d038c8ada3db2457608e2275b3e08d4"
-                    );
-                } else if (!web3.utils.isAddress(tokenized[1])) {
-                    bot.sendMessage(chatId, "invalid address");
-                } else {
-                    console.log("before");
-                    const cache = myCache.get("targetAddress");
-                    console.log("after:", cache);
-                    const idx = cache.findIndex((x) => x.address == tokenized[1]);
-                    console.log(idx);
-                    if (idx != -1) {
-                        cache.splice(idx, 1);
-                        myCache.set("targetAddress", cache);
-                        console.log("address: ", tokenized[1], " deleted!");
-                    }
-                    bot.sendMessage(chatId, "address deleted!");
-                }
-            }
+    fs.readFile("./groupIds.json", "utf8", (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        const json = JSON.parse(data);
+        if (!json.includes(chatId)) {
+          json.push(chatId);
+          fs.writeFileSync("./groupIds.json", JSON.stringify(json));
         }
-
-        // send a message to the chat acknowledging receipt of their message
-        // bot.sendMessage(chatId, `${chatId} Received your message`);
+      }
     });
-    web3.eth.subscribe("pendingTransactions", async (error, result) => {
-        if (error) {
-            console.log(error);
+
+    if (msg.text) {
+      const tokenized = msg.text.split(" ");
+      if (tokenized[0] == "/add") {
+        if (tokenized.length != 3) {
+          bot.sendMessage(
+            chatId,
+            "invalid command i.e. /add ElonMusk 0x4d9361a86d038c8ada3db2457608e2275b3e08d4"
+          );
+        } else if (
+          myCache.get("targetAddress").filter((x) => x.address == tokenized[2])
+            .length != 0
+        ) {
+          bot.sendMessage(chatId, "address already in database");
+        } else if (!web4.utils.isAddress(tokenized[2])) {
+          bot.sendMessage(chatId, "invalid address");
         } else {
-            // const rst = await Promise.all([web4.eth.getTransaction(result), web4.eth.getTransactionReceipt(result)])
-            // const tx = rst[0]
-            // const receipt = rst[1]
-
-            let targetAddress = myCache.get("targetAddress");
-
-            const tx = await web4.eth.getTransaction(result);
-            const receipt = await web4.eth.getTransactionReceipt(result);
-            try {
-                if (targetAddress.map((x) => x.address).includes(tx.from)) {
-                    const target = targetAddress.filter((x) => x.address == tx.from)[0];
-
-                    const data = abiDecoder.decodeMethod(tx.input);
-                    if (receipt.status && data && data.name.includes("swap")) {
-                        let logs = abiDecoder.decodeLogs(receipt.logs);
-                        logs = logs.filter((log) => log.name == "Transfer");
-                        console.log(tx.from);
-
-                        const path =
-                            data.params[data.params.findIndex((x) => x.name == "path")].value;
-                        const inTokenContract = new web4.eth.Contract(erc20Abi, path[0]);
-                        const outTokenContract = new web4.eth.Contract(
-                            erc20Abi,
-                            path[path.length - 1]
-                        );
-
-                        const inTokenName = await inTokenContract.methods.symbol().call();
-                        const outTokenName = await outTokenContract.methods.symbol().call();
-
-                        const inAmount =
-                            inTokenName == "USDC" || inTokenName == "fUSDT"
-                                ? web3.utils.fromWei(logs[0].events[2].value, "mwei")
-                                : web3.utils.fromWei(logs[0].events[2].value, "ether");
-                        const outAmount =
-                            outTokenName == "USDC" || outTokenName == "fUSDT"
-                                ? web3.utils.fromWei(
-                                    logs[logs.length - 1].events[2].value,
-                                    "mwei"
-                                )
-                                : web3.utils.fromWei(
-                                    logs[logs.length - 1].events[2].value,
-                                    "ether"
-                                );
-                        console.log("intoken: ", inTokenName, inAmount);
-                        console.log("outToken: ", outTokenName, outAmount);
-
-                        const targetGroupIds = require("./groupIds.json");
-
-                        for (id of targetGroupIds) {
-                            bot.sendMessage(
-                                id,
-                                `${target.name}\n${target.address} \nsell: ${inTokenName} ${inAmount}\nbuy: ${outTokenName} ${outAmount}\nhttps://ftmscan.com/tx/${result}`
-                            );
-                        }
-                    }
-                }
-
-            } catch (e) {
-                console.log(e)
-            }
+          const cache = myCache.get("targetAddress");
+          cache.push({
+            name: tokenized[1],
+            address: tokenized[2],
+          });
+          myCache.set("targetAddress", cache);
+          console.log("address: ", tokenized[2], " added!");
+          bot.sendMessage(chatId, "address added!");
         }
-    });
+      } else if (tokenized[0] == "/delete") {
+        if (tokenized.length != 2) {
+          bot.sendMessage(
+            chatId,
+            "invalid command i.e. /delete 0x4d9361a86d038c8ada3db2457608e2275b3e08d4"
+          );
+        } else if (!web4.utils.isAddress(tokenized[1])) {
+          bot.sendMessage(chatId, "invalid address");
+        } else {
+          console.log("before");
+          const cache = myCache.get("targetAddress");
+          console.log("after:", cache);
+          const idx = cache.findIndex((x) => x.address == tokenized[1]);
+          console.log(idx);
+          if (idx != -1) {
+            cache.splice(idx, 1);
+            myCache.set("targetAddress", cache);
+            console.log("address: ", tokenized[1], " deleted!");
+          }
+          bot.sendMessage(chatId, "address deleted!");
+        }
+      }
+    }
+
+    // send a message to the chat acknowledging receipt of their message
+    // bot.sendMessage(chatId, `${chatId} Received your message`);
+  });
+
+  let currentBlockNumber = await web4.eth.getBlockNumber()
+  setInterval(async()=>{
+    const newBlockNumber = await web4.eth.getBlockNumber()
+    console.log("new block number: ",newBlockNumber)
+
+    if (newBlockNumber!=currentBlockNumber){
+      for (let i=currentBlockNumber;i<newBlockNumber;i++){
+        await scanBlock(i, myCache)
+        console.log("block: ",i," finished scanning!")
+      }
+      currentBlockNumber = newBlockNumber
+    }
+  },1000)
+
+  // web4.eth.subscribe("pendingTransactions", async (error, result) => {
+  //   if (error) {
+  //     console.log(error);
+  //   } else {
+  //     // const rst = await Promise.all([web4.eth.getTransaction(result), web4.eth.getTransactionReceipt(result)])
+  //     // const tx = rst[0]
+  //     // const receipt = rst[1]
+
+  //     let targetAddress = myCache.get("targetAddress");
+
+  //     const tx = await web4.eth.getTransaction(result);
+  //     const receipt = await web4.eth.getTransactionReceipt(result);
+  //     try {
+  //       if (targetAddress.map((x) => x.address).includes(tx.from)) {
+  //         const target = targetAddress.filter((x) => x.address == tx.from)[0];
+
+  //         const data = abiDecoder.decodeMethod(tx.input);
+  //         if (receipt.status && data && data.name.includes("swap")) {
+  //           let logs = abiDecoder.decodeLogs(receipt.logs);
+  //           logs = logs.filter((log) => log.name == "Transfer");
+  //           console.log(tx.from);
+
+  //           const path =
+  //             data.params[data.params.findIndex((x) => x.name == "path")].value;
+  //           const inTokenContract = new web4.eth.Contract(erc20Abi, path[0]);
+  //           const outTokenContract = new web4.eth.Contract(
+  //             erc20Abi,
+  //             path[path.length - 1]
+  //           );
+
+  //           const inTokenName = await inTokenContract.methods.symbol().call();
+  //           const outTokenName = await outTokenContract.methods.symbol().call();
+
+  //           const inAmount =
+  //             inTokenName == "USDC" || inTokenName == "fUSDT"
+  //               ? web4.utils.fromWei(logs[0].events[2].value, "mwei")
+  //               : web4.utils.fromWei(logs[0].events[2].value, "ether");
+  //           const outAmount =
+  //             outTokenName == "USDC" || outTokenName == "fUSDT"
+  //               ? web4.utils.fromWei(
+  //                   logs[logs.length - 1].events[2].value,
+  //                   "mwei"
+  //                 )
+  //               : web4.utils.fromWei(
+  //                   logs[logs.length - 1].events[2].value,
+  //                   "ether"
+  //                 );
+  //           console.log("intoken: ", inTokenName, inAmount);
+  //           console.log("outToken: ", outTokenName, outAmount);
+
+  //           const targetGroupIds = require("./groupIds.json");
+
+  //           for (id of targetGroupIds) {
+  //             bot.sendMessage(
+  //               id,
+  //               `${target.name}\n${target.address} \nsell: ${inTokenName} ${inAmount}\nbuy: ${outTokenName} ${outAmount}\nhttps://ftmscan.com/tx/${result}`
+  //             );
+  //           }
+  //         }
+  //       }
+  //     } catch (e) {
+  //       console.log(e);
+  //     }
+  //   }
+  // });
 }
 
 main();
